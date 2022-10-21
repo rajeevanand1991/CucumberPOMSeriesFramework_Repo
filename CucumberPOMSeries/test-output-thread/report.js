c9428185ae7676c49262fe6b038fbb9bf31a5e29
rajeevanand1991 <rajeevanand1991@gmail.com>
2022-10-21 21:34:36 +0530

    To make run in TestNG with parallel execution

--- CucumberPOMSeries/test-output-thread/report.js
+++ CucumberPOMSeries/test-output-thread/report.js
@@ -1,65 +1,111 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "f52e5e09-74c1-4bf9-8402-ea5b95a63c63",
+    "id": "2fde325a-811d-461d-a329-2b5c635ffd00",
     "feature": "Account Page Feature",
     "scenario": "Accounts section count",
-    "start": 1666193238361,
-    "group": 1,
+    "start": 1666366891664,
+    "group": 22,
     "content": "",
     "tags": "@accounts,",
-    "end": 1666193268096,
+    "end": 1666366949946,
     "className": "passed"
   },
   {
-    "id": "2b0d30aa-9e73-42b4-aeba-cf06589bc581",
-    "feature": "Login page feature",
-    "scenario": "Login page title",
-    "start": 1666193268108,
-    "group": 1,
+    "id": "5c1dd00a-44ce-4888-8c66-708b94f55cdf",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1666366891665,
+    "group": 21,
     "content": "",
-    "tags": "",
-    "end": 1666193286257,
+    "tags": "@accounts,",
+    "end": 1666366950382,
     "className": "passed"
   },
   {
-    "id": "98155c77-7a79-4d38-8d60-b16f021e5e2c",
+    "id": "7da638b8-edcb-465d-8ecd-9c58a6894c6d",
     "feature": "Login page feature",
     "scenario": "Login with correct credentials",
-    "start": 1666193300722,
-    "group": 1,
+    "start": 1666366891700,
+    "group": 20,
     "content": "",
     "tags": "",
-    "end": 1666193318432,
+    "end": 1666366950006,
     "className": "passed"
   },
   {
-    "id": "0ed6f806-82a0-4d4d-8289-dd939e6f1adf",
+    "id": "0c40f8eb-fbaf-4ae6-9e60-397972131652",
     "feature": "Login page feature",
     "scenario": "Forgot Password link",
-    "start": 1666193286310,
-    "group": 1,
+    "start": 1666366891522,
+    "group": 19,
     "content": "",
     "tags": "",
-    "end": 1666193300702,
+    "end": 1666366940388,
     "className": "failed"
   },
   {
-    "id": "abaf8108-e10b-47b7-a062-2674eda875d7",
+    "id": "4d23b34c-1522-4eb3-adb9-51fb1a43242a",
     "feature": "Account Page Feature",
     "scenario": "Accounts page title",
-    "start": 1666193211367,
-    "group": 1,
+    "start": 1666366891664,
+    "group": 16,
     "content": "",
     "tags": "@accounts,",
-    "end": 1666193238338,
+    "end": 1666366950317,
+    "className": "passed"
+  },
+  {
+    "id": "7c51d6a9-ae93-4327-806e-47dcf9c85258",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts section count",
+    "start": 1666366891695,
+    "group": 17,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1666366950069,
+    "className": "passed"
+  },
+  {
+    "id": "05364ea0-d382-4b5f-8f91-4a5845eab602",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1666366891696,
+    "group": 18,
+    "content": "",
+    "tags": "",
+    "end": 1666366940282,
     "className": "passed"
   }
 ]);
 CucumberHTML.timelineGroups.pushArray([
   {
-    "id": 1,
-    "content": "Thread[main,5,main]"
+    "id": 16,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  },
+  {
+    "id": 17,
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  },
+  {
+    "id": 18,
+    "content": "Thread[TestNG-PoolService-2,5,main]"
+  },
+  {
+    "id": 19,
+    "content": "Thread[TestNG-PoolService-3,5,main]"
+  },
+  {
+    "id": 20,
+    "content": "Thread[TestNG-PoolService-4,5,main]"
+  },
+  {
+    "id": 21,
+    "content": "Thread[TestNG-PoolService-5,5,main]"
+  },
+  {
+    "id": 22,
+    "content": "Thread[TestNG-PoolService-6,5,main]"
   }
 ]);
 });
